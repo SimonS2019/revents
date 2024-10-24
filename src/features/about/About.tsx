bbccfcb689215759d7db930d6996ee3466b4882e
SimonS2019 <shentiexiong@gmail.com>
2024-10-25 09:37:24 +1300

    Refactor About page content and add contact information

--- src/features/about/About.tsx
+++ src/features/about/About.tsx
@@ -1,19 +1,43 @@
-import { Container, Header, Segment } from 'semantic-ui-react';
+import { Container, Header, Segment } from "semantic-ui-react";
 
 export default function About() {
-    return (
-        <Container>
-            <Segment>
-                <Header as='h1'>About Us</Header>
-                <p>
-                    Welcome to Re-vents! We are a platform dedicated to organizing and managing events.
-                    Our mission is to make event management seamless and efficient for everyone.
-                </p>
-                <p>
-                    Whether you are planning a small gathering or a large conference, Re-vents provides
-                    the tools you need to ensure your event is a success. Thank you for choosing Re-vents!
-                </p>
-            </Segment>
-        </Container>
-    );
+  return (
+    <Container>
+      <Segment>
+        <Header as="h1">About this project</Header>
+        <p>
+          This application is created by following the instructions from Neil
+          Cummings' course. It does not contain any of my original content. The
+          main purpose of this project is to help me learn how to implement
+          various technologies such as Firebase, React, and Redux, as
+          demonstrated in the course.
+        </p>
+        <p>
+          I plan to use Firebase and Angular in the future to build my own
+          applications with original content. This project serves as a
+          foundation for understanding the underlying concepts and technologies.
+        </p>
+        <p>
+          About me: I am a software engineer based in New Zealand. If you have
+          any questions or inquiries, feel free to contact me:
+        </p>
+        <p>
+          Email:{" "}
+          <a href="mailto:shentiexiong@gmail.com">shentiexiong@gmail.com</a>
+        </p>
+        <p>Phone: +64 22 122 0936</p>
+        <p>
+          <strong>GitHub:</strong>{" "}
+          <a
+            href="https://github.com/SimonS2019"
+            target="_blank"
+            rel="noopener noreferrer"
+          >
+            https://github.com/SimonS2019
+          </a>
+        </p>
+        <p>Date: 25/10/2024</p>
+      </Segment>
+    </Container>
+  );
 }
